b5d5c53478fbe2542e625928656aebe6de0a2f26
aliceburkhardt <40422452+aliceburkhardt@users.noreply.github.com>
2018-08-03 22:26:57 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var path = './';
+
+gulp.task('sass', function(){
+    gulp.src(path + '/scss/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest(path + '/css'));
+});
+
+gulp.task('sass:watch', function(){
+    gulp.watch('./' + path + '/scss/*.scss', ['sass']);
+});
+gulp.watch('main.css', function(){
+    console.log('seen');
+});
